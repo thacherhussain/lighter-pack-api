3a63989aec93d371a401fe42d2cdec48960bcb0c
Thacher Hussain <thacherT1D@gmail.com>
2019-05-30 01:15:34 -0700

    update index to express template

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
-var gear = require("./gear");
-var categories = require("./categories");
+var express = require('express');
+var router = express.Router();
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  'use strict';
 
-module.exports = {
-  gear: gear,
-  categories: categories
-};
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+// 
+// var gear = require("./gear");
+// var categories = require("./categories");
+//
+//
+// module.exports = {
+//   gear: gear,
+//   categories: categories
+// };
